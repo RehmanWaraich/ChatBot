b21dbf03ad27393ffc35eff2f82afee91453f324
RehmanWaraich <rehmanwaraich2023@gmail.com>
2023-10-31 23:15:12 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,48 @@
+import openai
+import streamlit as st
+import pandas as pd
+import numpy as np
+import math
+import random
+
+st.set_page_config(
+    page_title="ChatGPT",
+    page_icon="icon/favicon.png",
+)
+
+st.markdown("<h1 style='text-align: center;'>ChatGPT</h1><p style='text-align: center;'>Made by Rehman Waraich ❤</p>", unsafe_allow_html=True)
+
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+
